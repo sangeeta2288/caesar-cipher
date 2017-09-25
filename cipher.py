1e2f26797e61845bfd93ed43536b72e01ff0a37b
sangeeta2288 <ashrit.sangeeta@gmail.com>
2017-09-25 12:27:43 -0400

    init

---
+++ cipher.py
@@ -0,0 +1,9 @@
+# ---------- PROBLEM : CAESAR'S CIPHER ----------
+# Receive a message and then encrypt it by shifting the
+# characters by a requested amount('key' from user) to the right
+
+
+message = input(print("Enter your message"))
+key = int(input(print("Enter Key(No. to shift each charecter:1-26)")))
+print(message)
+print(key)
